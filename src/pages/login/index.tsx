1bf85605553409ef8f5a5f5c27322e4a6bea5b78
Ananta Pirdhaus <ananta.firdaus2@gmail.com>
2026-06-21 14:24:54 +0700

    fix: reCaptcha back to old version

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -220,7 +220,7 @@ export default function LoginPage() {
                 {process.env.NODE_ENV === "production" && (
                   <ReCAPTCHA
                     ref={recaptchaRef}
-                    sitekey={"6LdySCstAAAAAEmdkOdX_EDSpdCvMEtMRfVnHuMi"}
+                    sitekey={"6LeOhmErAAAAAB7DKR37KWi1cDSfupCmnQv9xcV6"}
                     className="flex justify-center"
                   />
                 )}
